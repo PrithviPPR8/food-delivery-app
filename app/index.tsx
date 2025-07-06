b0ac1225e185c648828e7faad16ca61007d09af8
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-07-07 02:08:15 +0530

    First commit. Added Native Wind to Project. Made the fonts work.

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+ 
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center text-primary font-quicksand-bold">
+        Welcome to my React Native App!
+      </Text>
+      <Text className="text-5xl text-center text-blue-500 font-manufacturingconsent-regular mt-6">
+        Testing a different font
+      </Text>
+    </View>
+  );
+}
+
+
+
