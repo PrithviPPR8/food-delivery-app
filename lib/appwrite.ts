ff589494e77f05c953b50c43efaf2fcae1f215e8
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-07-20 09:47:05 +0530

    Appwrite setup done

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.jsm.foodordering",
+    databaseId: "686f50cc0033c431bf45",
+    userCollectionId: "686f526900287ea064ec",
+}
